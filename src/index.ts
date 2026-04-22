597dd4f2693a91e0f03e636c19a0875554d966f8
claytonzhuo <eafanzhuo@foxmail.com>
2026-04-22 18:10:52 +0800

    exports sum

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,10 @@
 // const pel = new MathIt.People("fg");
 // console.log(MathIt.PI);
 import interval from "./utils/interval/index.cjs";
+// import { sum } from "./utils/sum/index.js";
+
+// TypeScript: Restart TS server 来重启server，如果类型提示没生效的话
+// console.log(sum(1, 2));
 
 const { getArrayLength, maxInterval } = interval;
 
---
+++ src/utils/sum/index.js
@@ -0,0 +1,8 @@
+/**
+ * 求和
+ * @param  {number[]} args 待求和项
+ * @returns {number} 和
+ */
+export function sum(...args) {
+  return Math.sum(...args);
+}
