274c3de9ee966244517808aec3f99dfabb23820f
claytonzhuo <eafanzhuo@foxmail.com>
2026-05-03 12:04:42 +0800

    Delete redundancy

--- src/mf/host.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 加载远程模块
- */
-export async function load() {
-  await import("./remote.js");
-
-  mf_employee.mount("hello world");
-}
--- src/types/widget-base.d.ts
+++
@@ -1,6 +0,0 @@
-interface Widget {
-  id: number;
-}
-
-declare function getWidget(id: number): Widget;
-declare function getWidget(content: string): Widget[];
