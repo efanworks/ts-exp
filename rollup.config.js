ca507c26ef2dc7c838a1d64203908050567e18de
claytonzhuo <eafanzhuo@foxmail.com>
2026-05-05 17:22:56 +0800

    修改打包结构

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,28 @@ import resolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.ts",
-  output: {
-    file: "dist/bundle.js",
-    format: "es",
-  },
+  output: [
+    {
+      dir: "dist/esm",
+      format: "esm",
+      preserveModules: true,
+      preserveModulesRoot: "src",
+      entryFileNames: "[name].mjs",
+    },
+    {
+      dir: "dist/cjs",
+      format: "cjs",
+      preserveModules: true,
+      preserveModulesRoot: "src",
+      entryFileNames: "[name].cjs",
+    },
+  ],
   plugins: [
     resolve({ extensions: [".js", ".ts"] }),
-    babel({ extensions: [".js", ".ts"], babelHelpers: "bundled" }),
+    babel({
+      extensions: [".js", ".ts"],
+      presets: ["@babel/preset-env", "@babel/preset-typescript"],
+      babelHelpers: "bundled",
+    }),
   ],
 };
